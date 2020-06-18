23205b0c9e3648271e4f2814c75630ce7f6efc9a
samiul <samiulhoque67@gmail.com>
2020-06-19 03:21:54 +0600

    stack updated

--- stack.py
+++ stack.py
@@ -6,12 +6,14 @@ class Stack:
     def push(self,value):
         if(self.top==self.size-1):
             print("overflow")
+            return
         
         self.top+=1
         self.st[self.top]=value
     def pop(self):
         if(self.top==-1):
-            print("underflow")  
+            print("underflow")
+            return
         lastitem=self.st[self.top]
         self.top-=1
         return lastitem
@@ -23,7 +25,7 @@ class Stack:
         if(self.top==-1):
             print("no number in stack\n")
             return
-        print(','.join(str(self.st[i]) for i in range(0,self.top+1)))
+        print(','.join(str(self.st[i]) for i in range(self.top,-1,-1)))
         
 
 
@@ -33,7 +35,7 @@ stack=Stack(size)
 while(1):
     n=int(input("type 1 for push \ntype 2 for pop \ntype 3 for print\ntype 4 for quit\ntype : "))
     if n==1:
-        m=int(input("give a numer :"))
+        m=int(input("give a number :"))
         stack.push(m)
     elif n==2:
         p=stack.pop()
